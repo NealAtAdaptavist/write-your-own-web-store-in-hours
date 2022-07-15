3aae56e6d0bac73d3c36c4fcdfd8de8436d0fd10
NealAtAdaptavist <nriley@adaptavist.com>
2022-07-15 22:12:00 +0000

    added miro board init

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+async function init() {
+  // Listen for a click on your app icon and open /app when clicked
+  miro.board.ui.on('icon:click', async () => {
+    await miro.board.ui.openModal({
+      url: 'index.html',
+      width: 800,
+      height: 600,
+      fullscreen: false,
+    });
+  });
+}
+
+init();
+
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider domain="dev-p9p797fk.us.auth0.com" clientId="gFdf1HX3He3G2Jz0YYoy8uqbMLLQtXJV" redirectUri={window.location.origin}>
