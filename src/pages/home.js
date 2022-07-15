d7c2fc71b06760ba6a0018ab1ed10db00a7fd95d
NealAtAdaptavist <nriley@adaptavist.com>
2022-07-15 22:49:23 +0000

    added access token

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const BuyNowButton = () => {
-  const { isLoading, isAuthenticated, loginWithPopup } = useAuth0();
+const BuyNowButton = async () => {
+  const { isLoading, isAuthenticated, loginWithPopup, getAccessTokenSilently} = useAuth0();
 
   const buy = () => {};
 
   if (isLoading) return <></>;
 
   if (isAuthenticated) return <button onClick={buy}>Buy Now</button>;
+
+  const access_token = await getAccessTokenSilently();
+
+  console.log(access_token)
 
   return <button onClick={loginWithPopup}>Log In To Purchase</button>;
 };
