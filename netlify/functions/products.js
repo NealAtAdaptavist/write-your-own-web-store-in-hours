8b4f242d223e6431b9016a83bf9a6b3bcd15e3e2
NealAtAdaptavist <nriley@adaptavist.com>
2022-07-18 21:41:14 +0000

    split out mysql connection

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -16,7 +16,7 @@ exports.handler = verifyJwt(async function (event, context) {
     if (payload.boardInfo.id && payload.userInfo.id) {
       let boardQuery = `INSERT IGNORE into tenant (auth_sub, auth_provider, auth_sub_id, board_id, user_id) VALUES ("${userSub}","${subParts[0]}","${subParts[1]}", "${payload.boardInfo.id}", "${payload.userInfo.id}")`
       console.log(boardQuery)
-      const connection = mysql.createConnection(`${process.env.APP_DATABASE_URL}?ssl={"rejectUnauthorized":true}`);
+      const connection = mysql.createConnection(`mysql://${process.env.APP_DATABASE_USER}:${process.env.APP_DATABASE_PASS}@${process.env.APP_DATABASE_DOMAIN}/${process.env.APP_DATABASE_PATH}?ssl={"rejectUnauthorized":true}`);
       connection.query(boardQuery, function (err, result, fields) {
         if (err) throw err;
         console.log(result);      
