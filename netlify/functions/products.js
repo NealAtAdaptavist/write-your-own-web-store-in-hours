f4064136406cee507d7e342af2f89ce263d1fa9d
NealAtAdaptavist <nriley@adaptavist.com>
2022-07-18 21:54:58 +0000

    print out connections

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -16,10 +16,13 @@ exports.handler = verifyJwt(async function (event, context) {
     if (payload.boardInfo.id && payload.userInfo.id) {
       let boardQuery = `INSERT IGNORE into tenant (auth_sub, auth_provider, auth_sub_id, board_id, user_id) VALUES ("${userSub}","${subParts[0]}","${subParts[1]}", "${payload.boardInfo.id}", "${payload.userInfo.id}")`
       console.log(boardQuery)
+      console.log(`mysql://${process.env.APP_DATABASE_USER}:${process.env.APP_DATABASE_PASS}@${process.env.APP_DATABASE_DOMAIN}/${process.env.APP_DATABASE_PATH}?ssl={"rejectUnauthorized":true}`)
       const connection = mysql.createConnection(`mysql://${process.env.APP_DATABASE_USER}:${process.env.APP_DATABASE_PASS}@${process.env.APP_DATABASE_DOMAIN}/${process.env.APP_DATABASE_PATH}?ssl={"rejectUnauthorized":true}`);
-      connection.query(boardQuery, function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);      
+      connection.query(boardQuery, function (err, result) {
+        if (err) {
+          console.warn(err)
+        }
+        else console.log(result);      
       });
       connection.end();
     }
