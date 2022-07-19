64381f8543600ddd4b254da5bd829d5e5137058a
NealAtAdaptavist <nriley@adaptavist.com>
2022-07-19 08:43:35 +0000

    hes gone and done it

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -1,9 +1,5 @@
 const { NetlifyJwtVerifier } = require("@serverless-jwt/netlify");
 
-// Starting
-var Analytics = require('analytics-node');
-
-
 
 const verifyJwt = NetlifyJwtVerifier({
   issuer: process.env.AUTH0_ISSUER,
@@ -13,29 +9,10 @@ const verifyJwt = NetlifyJwtVerifier({
 const waitingFunc = async (event, context) => {
   let payload = JSON.parse(event.body);
   payload.context = context.identityContext
-  let resp
    if (payload.context.claims) {
     let userSub = payload.context.claims.sub
     let subParts = userSub.split("|")
-    if (payload.boardInfo && payload.userInfo) {
-      console.log("Throwing track call")
-      resp = analytics.identify({
-        userId: subParts[1],
-        traits: {
-          miroId: payload.userInfo.id,
-          provider: subParts[0],
-          createdAt: new Date()
-        }
-      });
-      console.log(resp)
-      resp = analytics.track({
-        userId: subParts[1],
-        event: 'Viewed Board',
-        properties: {
-          boardId: payload.boardInfo.id
-        }
-      });
-      console.log(resp)
+    if (payload.boardInfo && payload.userInfo) {      
       let boardQuery = `INSERT IGNORE into tenant (auth_sub, auth_provider, auth_sub_id, board_id, user_id) VALUES ("${userSub}","${subParts[0]}","${subParts[1]}", "${payload.boardInfo.id}", "${payload.userInfo.id}")`
       console.log(boardQuery)
       console.log(`mysql://${process.env.APP_DATABASE_USER}:${process.env.APP_DATABASE_PASS}@${process.env.APP_DATABASE_DOMAIN}/${process.env.APP_DATABASE_PATH}?ssl={"rejectUnauthorized":true}`)
@@ -48,72 +25,50 @@ const waitingFunc = async (event, context) => {
             body: JSON.stringify({er: err}),
           };
         }
-        else console.log(result);      
+        else {
+          console.log(result); 
+          return {
+            statusCode: 403,
+            body: JSON.stringify({result: result}),
+          };
+        }     
       });
       connection.end();
     } else {
-      resp=analytics.identify({
-        userId: subParts[1],
-        traits: {
-          provider: subParts[0],
-          createdAt: new Date()
-        }
-      });
-      console.log(resp)
+      return {
+        statusCode: 200,
+        body: JSON.stringify({er: 'no user found'}),
+      };
     }
   }
-
-  await analytics.flush(function(err) {
-    console.log('Flushed, and now this program can exit!')
-    if (err) console.log(err)
-  })
-  return (resp)
 }
 
-
-const boringFunction = async (event, context) => {
-  analytics.identify({
-    userId: '12345',
-    traits: {
-      miroId: '5678',
-      provider: 'i am sad',
-      createdAt: new Date()
-    }
-  });
-  const butseriously = await analytics.flush(function(err) {
-    console.log('Flushed, and now this program can exit!')
-    if (err) return({err: err})
-  })
-  return {succes: butseriously}
-};
-
-async function lambda()
-{
-  const WRITE_KEY = 'BlFUAAGcnOCCewCIEVFvEDxJSt2Uvoyu'
-  const analytics = new Analytics(WRITE_KEY, { flushAt: 20 });
-  analytics.flushed = true;
+// async function lambda()
+// {
+//   const WRITE_KEY = 'BlFUAAGcnOCCewCIEVFvEDxJSt2Uvoyu'
+//   const analytics = new Analytics(WRITE_KEY, { flushAt: 20 });
+//   analytics.flushed = true;
   
-  analytics.track({
-    anonymousId: '1111111',
-    event: 'Test event',
-    properties: {
-      name: 'Test event',
-      timestamp: new Date()
-    }
-  });
-  await analytics.flush(function(err, batch) {
-    console.log('Flushed, and now this program can exit!');
-    return ({in: 'here'})
-  });
-}
+//   analytics.track({
+//     anonymousId: '1111111',
+//     event: 'Test event',
+//     properties: {
+//       name: 'Test event',
+//       timestamp: new Date()
+//     }
+//   });
+//   await analytics.flush(function(err, batch) {
+//     console.log('Flushed, and now this program can exit!');
+//     return ({in: 'here'})
+//   });
+// }
 
-// exports.handler = verifyJwt( async function (event, context) {
+exports.handler = verifyJwt( async function (event, context) {
   // Decode the payload
-  exports.handler =  async function (event, context) {
-  const resp = await lambda()
+  const resp = await waitingFunc(event, context)
   return {
     statusCode: 200,
-    body: JSON.stringify({resp: resp, ts: new Date()}),
+    body: resp,
   };
 
-};
+});
