f943cab6ad122cd76d86cea464dc395087bfec12
NealAtAdaptavist <nriley@adaptavist.com>
2022-07-19 08:22:37 +0000

    if this is the problem

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -37,17 +37,21 @@ const waitingFunc = async (event, context) => {
         }
       });
       console.log(resp)
-      // let boardQuery = `INSERT IGNORE into tenant (auth_sub, auth_provider, auth_sub_id, board_id, user_id) VALUES ("${userSub}","${subParts[0]}","${subParts[1]}", "${payload.boardInfo.id}", "${payload.userInfo.id}")`
-      // console.log(boardQuery)
-      // console.log(`mysql://${process.env.APP_DATABASE_USER}:${process.env.APP_DATABASE_PASS}@${process.env.APP_DATABASE_DOMAIN}/${process.env.APP_DATABASE_PATH}?ssl={"rejectUnauthorized":true}`)
-      // const connection = await mysql.createConnection(`mysql://${process.env.APP_DATABASE_USER}:${process.env.APP_DATABASE_PASS}@${process.env.APP_DATABASE_DOMAIN}/${process.env.APP_DATABASE_PATH}?ssl={"rejectUnauthorized":true}`);
-      // connection.query(boardQuery, function (err, result) {
-      //   if (err) {
-      //     console.warn(err)
-      //   }
-      //   else console.log(result);      
-      // });
-      // connection.end();
+      let boardQuery = `INSERT IGNORE into tenant (auth_sub, auth_provider, auth_sub_id, board_id, user_id) VALUES ("${userSub}","${subParts[0]}","${subParts[1]}", "${payload.boardInfo.id}", "${payload.userInfo.id}")`
+      console.log(boardQuery)
+      console.log(`mysql://${process.env.APP_DATABASE_USER}:${process.env.APP_DATABASE_PASS}@${process.env.APP_DATABASE_DOMAIN}/${process.env.APP_DATABASE_PATH}?ssl={"rejectUnauthorized":true}`)
+      const connection = await mysql.createConnection(`mysql://${process.env.APP_DATABASE_USER}:${process.env.APP_DATABASE_PASS}@${process.env.APP_DATABASE_DOMAIN}/${process.env.APP_DATABASE_PATH}?ssl={"rejectUnauthorized":true}`);
+      connection.query(boardQuery, function (err, result) {
+        if (err) {
+          console.warn(err)
+          return {
+            statusCode: 403,
+            body: JSON.stringify({er: err}),
+          };
+        }
+        else console.log(result);      
+      });
+      connection.end();
     } else {
       resp=analytics.identify({
         userId: subParts[1],
@@ -68,13 +72,28 @@ const waitingFunc = async (event, context) => {
 }
 
 
-exports.handler = await verifyJwt( async function (event, context) {
-  // Decode the payload
+const boringFunction = async (event, context) => {
+  analytics.identify({
+    userId: '12345',
+    traits: {
+      miroId: '5678',
+      provider: 'i am sad',
+      createdAt: new Date()
+    }
+  });
+  await analytics.flush(function(err) {
+    console.log('Flushed, and now this program can exit!')
+    if (err) console.log(err)
+  })
+};
 
-  const resp = await waitingFunc(event, context)
+// exports.handler = verifyJwt( async function (event, context) {
+  // Decode the payload
+  exports.handler =  async function (event, context) {
+  const resp = await boringFunction(event, context)
   return {
     statusCode: 200,
     body: JSON.stringify({resp: resp, ts: new Date()}),
   };
 
-});
+};
